d7b8d75b5cb032f7c22a5d3220ef9eae4b992b43
LucasTHz <lucasthomaz58@gmail.com>
2022-10-21 11:55:15 -0300

    declaracao dos vetores

--- gauss-geidel/src/main.rs
+++ gauss-geidel/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let a: [[f64; 3]; 3] = [[4.0, 0.24, -0.08], [0.09, 3.0, -0.15], [0.04, -0.08, 4.0]]; // equações
+    let b: [f64; 3] = [8.0, 9.0, 20.0]; // termo independente
+    let mut vet_inicial: [f64; 3] = [0.0, 0.0, 0.0]; // vetor inicial (chute)
+    const interacao_max: usize = 5; // número de iterações
+
+    loop {
+        for i in 0..b.len() {
+            x = b[i];
+            for j in 0..b.len() {
+                if i != j {
+                    x -= a[i][j] * vet_inicial[j];
+                }
+            }
+        }
+    }
 }
